0ad33d7357a9716905559152f846a9c1aa9762af
jun02160 <jun020216@naver.com>
2023-05-15 18:20:28 +0900

    initial commit

---
+++ src/main/java/sopt/org/CarrotServer/CarrotServerApplication.java
@@ -0,0 +1,13 @@
+package sopt.org.CarrotServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrotServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrotServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sopt/org/CarrotServer/CarrotServerApplicationTests.java
@@ -0,0 +1,13 @@
+package sopt.org.CarrotServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrotServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
