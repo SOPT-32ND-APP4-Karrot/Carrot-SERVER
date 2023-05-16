e80feccd236ab624cc908deea370f9ad5c2c9795
onpyeong <gsh447114@gmail.com>
2023-05-16 19:14:27 +0900

    [ADD] User 엔티티 구현

--- src/main/java/sopt/org/CarrotServer/domain/user/User.java
+++ src/main/java/sopt/org/CarrotServer/domain/user/User.java
@@ -1,4 +1,39 @@
 package sopt.org.CarrotServer.domain.user;
 
-public class User {
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import sopt.org.CarrotServer.domain.BaseTimeEntity;
+import sopt.org.CarrotServer.domain.sale.Sale;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private Double temperature;
+
+    @Column(nullable = false)
+    private String location;
+
+    @Column(nullable = false)
+    private String profileImgUrl;
+
+    //판매한 상품
+    @OneToMany(mappedBy = "user") //일대다(1:N)
+    private final List<Sale> sales = new ArrayList<>();
 }
