3f296a927946a9d61519a309ab5a3e214576ca37
onpyeong <gsh447114@gmail.com>
2023-05-19 00:43:01 +0900

    [FEAT] 홈_전체 상품 조회 API 구현

--- src/main/java/sopt/org/CarrotServer/domain/sale/SaleLikeId.java
+++ src/main/java/sopt/org/CarrotServer/domain/sale/SaleLikeId.java
@@ -1,17 +1,27 @@
 package sopt.org.CarrotServer.domain.sale;
 
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import java.io.Serializable;
 
 @Embeddable //엔티티클래스에서 사용할 수 있는 임베디드 클래스
 //엔티티 클래스에서 해당 객체를 가지고 있는 경우, 해당 객체의 속성을 엔티티 테이블에서 컬럼으로 매핑
+@NoArgsConstructor
 public class SaleLikeId implements Serializable {
     //객체를 바이트형태로 직렬화
     //JPA에서 엔티티를 데이터베이스에 저장 또는 조회 시 객체를 직렬화하여 수행
     //복합키를 구현한 클래스도 직렬화 가능해야 함
-    @Column
+    @Column(nullable = false)
     private Long userId;
-    @Column
+    @Column(nullable = false)
     private Long saleId;
+
+    @Builder
+    public SaleLikeId(Long userId, Long saleId){
+        this.userId = userId;
+        this.saleId = saleId;
+    }
 }
