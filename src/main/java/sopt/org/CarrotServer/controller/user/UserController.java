5d7feeb9624f2c6e72aebb0e4f2a89c6b8b3b6e4
onpyeong <gsh447114@gmail.com>
2023-05-19 00:42:24 +0900

    [FEAT] User 생성 API

--- src/main/java/sopt/org/CarrotServer/controller/user/UserController.java
+++ src/main/java/sopt/org/CarrotServer/controller/user/UserController.java
@@ -1,4 +1,25 @@
 package sopt.org.CarrotServer.controller.user;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sopt.org.CarrotServer.common.dto.ApiResponse;
+import sopt.org.CarrotServer.controller.user.dto.request.CreateUserRequestDto;
+import sopt.org.CarrotServer.exception.SuccessStatus;
+import sopt.org.CarrotServer.service.user.UserService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
 public class UserController {
+
+    private final UserService userService;
+
+    //유저 생성
+    @PostMapping("")
+    public ApiResponse<Long> createUser(@RequestBody final CreateUserRequestDto request) {
+        return ApiResponse.success(SuccessStatus.CREATE_USER_SUCCESS, userService.createUser(request));
+    }
 }
--- src/main/java/sopt/org/CarrotServer/service/user/UserService.java
+++ src/main/java/sopt/org/CarrotServer/service/user/UserService.java
@@ -1,4 +1,28 @@
 package sopt.org.CarrotServer.service.user;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import sopt.org.CarrotServer.controller.user.dto.request.CreateUserRequestDto;
+import sopt.org.CarrotServer.domain.user.User;
+import sopt.org.CarrotServer.infrastructure.user.UserRepository;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    //유저 생성
+    public Long createUser(CreateUserRequestDto request) {
+        User user = User.builder()
+                .nickname(request.getNickname())
+                .phone(request.getPhone())
+                .temperature(request.getTemperature())
+                .location(request.getLocation())
+                .profileImgUrl(request.getProfileImgUrl())
+                .build();
+
+        userRepository.save(user);
+        return user.getUserId();
+    }
 }
