2fd5c2f66d66775476c0a3303bf8b22bb8f17868
onpyeong <gsh447114@gmail.com>
2023-05-19 11:10:08 +0900

    [FEAT] POST 이미지 추가

--- src/main/java/sopt/org/CarrotServer/controller/sale/dto/request/CreateSaleRequestDto.java
+++ src/main/java/sopt/org/CarrotServer/controller/sale/dto/request/CreateSaleRequestDto.java
@@ -1,21 +1,23 @@
 package sopt.org.CarrotServer.controller.sale.dto.request;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class CreateSaleRequestDto {
     private String category;
     private Integer view;
     private String title;
     private String description;
-    private String saleImgUrl;
     private Boolean isUpdated;
     private Integer price;
     private Boolean isSuggest;
     private String status;
     private Boolean isDiscount;
     private Integer userId;
+    private MultipartFile saleImgUrl;
 }
